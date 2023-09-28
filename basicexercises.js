6bb2079aff0e08bfa2cfaa1b41537fe75f43d8df
Zai Builds <hello@zaibuilds.io>
2023-09-28 22:29:10 +0100

    studying loops

---
+++ basicexercises.js
@@ -0,0 +1,13 @@
+// 1 - Write a program to print numbers from 1 - 10 using a for loop
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+// 2 - Print even numbers between 1 and 20 using a for loop.
+
+for (let i = 0; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(i)
+    }
+};
