aa8d9d3aed5471a1edfea0a44d50c6aaeb2da617
Zai Builds <hello@zaibuilds.io>
2023-09-29 22:26:13 +0100

    study

--- basicexercises.js
+++ basicexercises.js
@@ -21,3 +21,51 @@ for (let num = 1; num <=100; num++) {
 };
 
 console.log(`The total sum is ${totalSum}`);
+
+// 4 - Calculate and print the factorial of a number using a for loop
+
+// let number = 4;
+
+// for (let i = 1; i<=5; i++) {
+//     number *= i;
+// };
+
+// console.log(`The factorial of 5 is ${number}`);
+
+// My fist attempt ^
+
+function calculateFactorial (n) {
+    if (n < 0) {
+        return "Factorials are not returned for negative numbers"
+    } else if ( n === 0 || n === 1) {
+        return 1;
+    } else {
+        let result = 1; 
+        for (let i = 2; i <= n; i++) {
+            result *= i;
+          }
+          return result;
+        }
+      };
+
+      const num = 5;
+      const result = calculateFactorial(num);
+      console.log(`The factorial of ${num} is ${result}`);   
+
+// 5 - Print the multiplication table of a number using a for loop.
+
+let multiplicationNumber = 4;
+
+for (let i = 1; i <= 12; i++) {
+    console.log(multiplicationNumber * i);
+}
+
+// 6 - Print numbers in reverse order from 10 to 1 using a for loop.
+
+let reverseOrderNumber = 1;
+
+for (let i = 10; i >= 0; i--) {
+    console.log(reverseOrderNumber * i);
+}
+
+// 7 - Write a program to check if a number is prime using a for loop.
