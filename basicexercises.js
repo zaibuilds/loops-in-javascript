9bb5d00960a224de02f8b8ed7df23ec72d7b87c7
Zai Builds <hello@zaibuilds.io>
2023-09-30 10:20:18 +0100

    study

--- basicexercises.js
+++ basicexercises.js
@@ -14,13 +14,13 @@ for (let i = 0; i <= 20; i++) {
 
 // 3 - Print the sum of numbers from 1 to 100 using a for loop.
 
-let totalSum = 0;
+// let totalSum = 0;
 
-for (let num = 1; num <=100; num++) {
-    totalSum += num;
-};
+// for (let num = 1; num <=100; num++) {
+//     totalSum += num;
+// };
 
-console.log(`The total sum is ${totalSum}`);
+// console.log(`The total sum is ${totalSum}`);
 
 // 4 - Calculate and print the factorial of a number using a for loop
 
@@ -34,23 +34,23 @@ console.log(`The total sum is ${totalSum}`);
 
 // My fist attempt ^
 
-function calculateFactorial (n) {
-    if (n < 0) {
-        return "Factorials are not returned for negative numbers"
-    } else if ( n === 0 || n === 1) {
-        return 1;
-    } else {
-        let result = 1; 
-        for (let i = 2; i <= n; i++) {
-            result *= i;
-          }
-          return result;
-        }
-      };
+// function calculateFactorial (n) {
+//     if (n < 0) {
+//         return "Factorials are not returned for negative numbers"
+//     } else if ( n === 0 || n === 1) {
+//         return 1;
+//     } else {
+//         let result = 1; 
+//         for (let i = 2; i <= n; i++) {
+//             result *= i;
+//           }
+//           return result;
+//         }
+//       };
 
-      const num = 5;
-      const result = calculateFactorial(num);
-      console.log(`The factorial of ${num} is ${result}`);   
+//       const num = 5;
+//       const result = calculateFactorial(num);
+//       console.log(`The factorial of ${num} is ${result}`);   
 
 // 5 - Print the multiplication table of a number using a for loop.
 
@@ -70,14 +70,85 @@ for (let i = 10; i >= 0; i--) {
 
 // 7 - Write a program to check if a number is prime using a for loop.
 
-function checkPrime (n) {
-    if (n === 1 || n === 0) {
-        return "This number cannot be a prime"
-    } else {
-        for (let i = 1; i < n; i ++) {
-            return n % i
-        }
-}
-}
+// My attempt 1
+// function checkPrime (n) {
+//     if (n === 1 || n === 0) {
+//         console.log("This cannot be a prime number")
+//     } else {
+//         for (let i = 1; i < n; i ++) {
+//            if (n % i === 1) {
+//             console.log("This is a prime number")
+//            }
+//         }
+// }
+// }
+// checkPrime(21);
 
-checkPrime(5);
+
+// ChatGPT
+
+function isPrime(num) {
+    // Handle special cases for 0 and 1
+    if (num <= 1) {
+      return false;
+    }
+  
+    // Check for divisibility from 2 to the square root of the number
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        return false; // It's not a prime number
+      }
+    }
+  
+    return true; // It's a prime number
+  }
+  
+  // Example usage:
+  let num = 17;
+  if (isPrime(num)) {
+    console.log(`${num} is a prime number.`);
+  } else {
+    console.log(`${num} is not a prime number.`);
+  }
+
+
+
+
+  /* In this code:
+  
+  We define a function called isPrime that takes a number as an argument.
+  We handle special cases for numbers less than or equal to 1, as they are not prime by definition.
+  We use a for loop to check for divisibility of the given number by all integers from 2 up to the square root of the number.
+  If we find any divisor between 2 and the square root, the number is not prime, and we return false.
+  If no divisors are found in the loop, we return true, indicating that the number is prime.
+  You can replace the const num value with the number you want to check for primality. */
+  
+  
+  // My second attempt
+
+  function checkPrime(number) {
+    // Handle special cases 1 and 0
+
+     if (number <= 1) {
+        return false;
+     }
+
+    // Check for divisibility from 2 to the square root of the number
+
+     for (let i = 2; i < Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            return false;  // It's not a prime number
+        }
+     }
+
+     return true; // It is a prime number
+  }
+  
+   // Example usage:
+  let number = 23;
+  if (isPrime(number)) {
+    console.log(`${number} is a prime number.`);
+  } else {
+    console.log(`${number} is not a prime number.`);
+  }
+  
