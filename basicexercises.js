9c8ad89c949ab7aa3ba0b79d471a061eace383a4
Zai Builds <hello@zaibuilds.io>
2023-10-03 21:17:49 +0100

    studying but still spun

--- basicexercises.js
+++ basicexercises.js
@@ -1,26 +1,26 @@
 // 1 - Write a program to print numbers from 1 - 10 using a for loop
 
-for (let i = 0; i <= 10; i++) {
-    console.log(i);
-}
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
 
 // 2 - Print even numbers between 1 and 20 using a for loop.
 
-for (let i = 0; i <= 20; i++) {
-    if (i % 2 === 0) {
-        console.log(i)
-    }
-};
+// for (let i = 0; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+// };
 
 // 3 - Print the sum of numbers from 1 to 100 using a for loop.
 
-let totalSum = 0;
+// let totalSum = 0;
 
-for (let num = 1; num <=100; num++) {
-    totalSum += num;
-};
+// for (let num = 1; num <=100; num++) {
+//     totalSum += num;
+// };
 
-console.log(`The total sum is ${totalSum}`);
+// console.log(`The total sum is ${totalSum}`);
 
 // 4 - Calculate and print the factorial of a number using a for loop
 
@@ -54,19 +54,19 @@ console.log(`The total sum is ${totalSum}`);
 
 // 5 - Print the multiplication table of a number using a for loop.
 
-let multiplicationNumber = 4;
+// let multiplicationNumber = 4;
 
-for (let i = 1; i <= 12; i++) {
-    console.log(multiplicationNumber * i);
-}
+// for (let i = 1; i <= 12; i++) {
+//     console.log(multiplicationNumber * i);
+// }
 
-// 6 - Print numbers in reverse order from 10 to 1 using a for loop.
+// // 6 - Print numbers in reverse order from 10 to 1 using a for loop.
 
-let reverseOrderNumber = 1;
+// let reverseOrderNumber = 1;
 
-for (let i = 10; i >= 0; i--) {
-    console.log(reverseOrderNumber * i);
-}
+// for (let i = 10; i >= 0; i--) {
+//     console.log(reverseOrderNumber * i);
+// }
 
 // 7 - Write a program to check if a number is prime using a for loop.
 
@@ -87,29 +87,29 @@ for (let i = 10; i >= 0; i--) {
 
 // ChatGPT
 
-function isPrime(num) {
-    // Handle special cases for 0 and 1
-    if (num <= 1) {
-      return false;
-    }
+// function isPrime(num) {
+//     // Handle special cases for 0 and 1
+//     if (num <= 1) {
+//       return false;
+//     }
   
-    // Check for divisibility from 2 to the square root of the number
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-      if (num % i === 0) {
-        return false; // It's not a prime number
-      }
-    }
+//     // Check for divisibility from 2 to the square root of the number
+//     for (let i = 2; i <= Math.sqrt(num); i++) {
+//       if (num % i === 0) {
+//         return false; // It's not a prime number
+//       }
+//     }
   
-    return true; // It's a prime number
-  }
+//     return true; // It's a prime number
+//   }
   
-  // Example usage:
-  let num = 17;
-  if (isPrime(num)) {
-    console.log(`${num} is a prime number.`);
-  } else {
-    console.log(`${num} is not a prime number.`);
-  }
+//   // Example usage:
+//   let num = 17;
+//   if (isPrime(num)) {
+//     console.log(`${num} is a prime number.`);
+//   } else {
+//     console.log(`${num} is not a prime number.`);
+//   }
 
 
 
@@ -126,30 +126,48 @@ function isPrime(num) {
   
   // My second attempt
 
-  function checkPrime(number) {
-    // Handle special cases 1 and 0
+  // function checkPrime(number) {
+  //   // Handle special cases 1 and 0
 
-     if (number <= 1) {
-        return false;
-     }
+  //    if (number <= 1) {
+  //       return false;
+  //    }
 
-    // Check for divisibility from 2 to the square root of the number
+  //   // Check for divisibility from 2 to the square root of the number
 
-     for (let i = 2; i < Math.sqrt(number); i++) {
-        if (number % i === 0) {
-            return false;  // It's not a prime number
-        }
-     }
+  //    for (let i = 2; i < Math.sqrt(number); i++) {
+  //       if (number % i === 0) {
+  //           return false;  // It's not a prime number
+  //       }
+  //    }
 
-     return true; // It is a prime number
-  }
+  //    return true; // It is a prime number
+  // }
   
    // Example usage:
-  let number = 23;
-  if (isPrime(number)) {
-    console.log(`${number} is a prime number.`);
-  } else {
-    console.log(`${number} is not a prime number.`);
-  }
+  // let number = 23;
+  // if (isPrime(number)) {
+  //   console.log(`${number} is a prime number.`);
+  // } else {
+  //   console.log(`${number} is not a prime number.`);
+  // }
   
 // 8 - Print the Fibonacci series using a for loop.
+
+function printFibonacci(n) {
+  let fib = [];
+  fib[0] = 0;
+  fib[1] = 1;
+
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+
+  for (let i = 0; i < n; i++) {
+    console.log(fib[i]);
+  }
+}
+
+// Specify the number of Fibonacci numbers you want to print
+const n = 6;
+printFibonacci(n);
